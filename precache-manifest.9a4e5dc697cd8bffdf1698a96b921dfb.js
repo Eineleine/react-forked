16b457dece7038c110a68eff5df9a81fee74d3a7
Eineleine <99676025+Eineleine@users.noreply.github.com>
2022-05-28 18:28:07 +0300

    Add files via upload

---
+++ precache-manifest.9a4e5dc697cd8bffdf1698a96b921dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c2fc6b5e0434427cb70935f274703b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "272f0b9a41ec34398f71",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "4d2767be334604967cba",
+    "url": "/static/js/2.e7a989e8.chunk.js"
+  },
+  {
+    "revision": "272f0b9a41ec34398f71",
+    "url": "/static/js/main.2da27d5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
